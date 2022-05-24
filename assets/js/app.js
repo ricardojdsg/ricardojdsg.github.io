e6cea3e3772e309a180fc7375318ae305da7f385
ricardojdsg <ricardojdsg@gmail.com>
2022-05-24 14:07:01 -0400

    redes

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+
+particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+  console.log('particles.js loaded - callback');
+});
