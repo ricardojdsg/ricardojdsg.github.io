b62c0674185c56d76f3a54d8efd9390d8ed23eca
ricardojdsg <ricardojdsg@gmail.com>
2022-06-25 23:35:57 -0400

    Corrección particlesJS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 
+
+
+
 particlesJS.load('particles-js', '/particlesjs-config.json', function() {
   console.log('particles.js loaded - callback');
 });
+
+
